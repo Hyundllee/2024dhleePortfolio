29677f27dc9b70978910a7a680e0a872ef2cccdb
donghyun lee <azazs0000@naver.com>
2024-07-16 16:48:14 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <BrowserRouter basename='react-myportfolio'>
+    <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Main portfolioList={portfolioList} />}>
