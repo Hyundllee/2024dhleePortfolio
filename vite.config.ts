d63b42a8f22d81fac28662dbfe7b77fd99af8104
donghyun lee <azazs0000@naver.com>
2024-07-16 15:54:07 +0900

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-myportfolio/',
   plugins: [react()],
-
+  base: '',
+  
   // SCSS 전역 사용
   css: {
     preprocessorOptions: {
