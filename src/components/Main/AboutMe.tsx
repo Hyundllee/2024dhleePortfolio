2f0dd9a3becb859f4736f14d8031996b3925350e
donghyun lee <azazs0000@naver.com>
2024-07-17 15:36:14 +0900

    first commit

--- src/components/Main/AboutMe.tsx
+++ src/components/Main/AboutMe.tsx
@@ -19,10 +19,10 @@ const AboutMe = () => {
               </p>
               <p>
                 UI 개발자로서 3년간 웹 에이전시와 자사 솔루션 실무를 기반으로 <br/> 
-                대규모 웹 사이트, 다수의 공공 프로젝트 경험과 다양한 직군의 개발자들을 만나<br/>
+                대규모 웹 사이트, 다수의 공공 프로젝트 경험과 다양한 직군의 개발자들을 만나 <br/>
                 어떠한 환경에서도 적응할 수 있는 원활한 소통 능력을 갖추고 있습니다. <br/>
               </p>
-              <p>프레임워크와 라이브러리를 적재적소에 활용하여 효율성을 극대화하고<br/>
+              <p>프레임워크와 라이브러리를 적재적소에 활용하여 효율성을 극대화하고 <br/>
                 사용자들을 사로잡을 수 있도록 하는 일에 자부심을 갖고 개발해 나아갑니다.</p>
             </div>
             <a href="https://www.rallit.com/resumes/8233@azazs000/%EC%9D%B4%EB%8F%99%ED%98%84" target='_blank' className='aboutMore'><FontAwesomeIcon icon={faChevronCircleDown}/><span>자기소개 더보기</span></a>
